10f74b7966986a683f96d515637d2b90c80d076c
Soumyajit Pal <soumyajit.pal@zensar.com>
2020-09-01 14:50:22 +0530

    Add Files

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -6,11 +6,58 @@ using System.Web;
 using System.Web.Mvc;
 using MovieCustomerWithAuthMVC_app.Models.ViewModel;
 using System.Data.Entity;
+using System.Net;
 
 namespace MovieCustomerWithAuthMVC_app.Controllers
 {
     public class CustomersController : Controller
     {
+        // GET: Customers
+        //private ApplicationDbContext _context;
+        //public CustomersController()
+        //{
+        //    _context = new ApplicationDbContext();
+        //}
+        //// GET: Customers
+        //public ActionResult Index()
+        //{
+        //    var customers = _context.Customers.Include(c=>c.MembershipType).ToList();
+        //    return View(customers);
+        //}
+        //public ActionResult Details(int id)
+        //{
+        //    var singleCustomer = _context.Customers.Include(c=>c.MembershipType).SingleOrDefault(c => c.Id == id);
+        //    if (singleCustomer == null)
+        //        return HttpNotFound();
+        //    return View(singleCustomer);
+        //}
+        //[HttpGet]
+        //public ActionResult New()  //display the form
+        //{
+        //    var membershipTypes = _context.MembershipTypes.ToList();
+        //    var viewModel = new NewCustomerViewModel
+        //    {
+        //        MembershipTypes = membershipTypes
+        //    };
+        //    return View(viewModel);
+        //}
+        //[HttpPost]
+        //public ActionResult Create(Customer customer)  //submit the form,parameter is of model
+        //                                               //must have parameter in Post method      //Its called Model Binding
+        //{
+        //    _context.Customers.Add(customer);
+        //    _context.SaveChanges();
+        //    return RedirectToAction("Index", "Customers");//Back to customer controller page
+        //}
+        //protected override void Dispose(bool disposing)
+        //{
+        //    _context.Dispose();
+        //}
+
+
+
+
+
         // GET: Customers
         private ApplicationDbContext _context;
         public CustomersController()
@@ -20,12 +67,12 @@ namespace MovieCustomerWithAuthMVC_app.Controllers
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = _context.Customers.Include(c=>c.MembershipType).ToList();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
             return View(customers);
         }
         public ActionResult Details(int id)
         {
-            var singleCustomer = _context.Customers.Include(c=>c.MembershipType).SingleOrDefault(c => c.Id == id);
+            var singleCustomer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
             if (singleCustomer == null)
                 return HttpNotFound();
             return View(singleCustomer);
@@ -40,17 +87,87 @@ namespace MovieCustomerWithAuthMVC_app.Controllers
             };
             return View(viewModel);
         }
+        //[HttpPost]
+        //public ActionResult Create(Customer customer)  //submit the form,parameter is of model
+        //                                               //must have parameter in Post method      //Its called Model Binding
+        //{
+        //    _context.Customers.Add(customer);
+        //    _context.SaveChanges();
+        //    return RedirectToAction("Index", "Customers");//Back to customer controller page
+        //}
+
+
         [HttpPost]
-        public ActionResult Create(Customer customer)  //submit the form,parameter is of model
-                                                       //must have parameter in Post method      //Its called Model Binding
+        public ActionResult Save(Customer customer)
         {
-            _context.Customers.Add(customer);
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new NewCustomerViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes=_context.MembershipTypes.ToList()
+                    
+                };
+                return View("New",viewModel);
+            }
+
+
+            if (customer.Id == 0)
+                _context.Customers.Add(customer);
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+                customerInDb.Name = customer.Name;
+                customerInDb.DOB = customer.DOB;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribe = customer.IsSubscribe;
+
+            }
             _context.SaveChanges();
             return RedirectToAction("Index", "Customers");//Back to customer controller page
+
         }
+
+
+
+        public ActionResult Edit(int id)
+        {
+            var updateCast = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (updateCast == null)
+            {
+                return HttpNotFound();
+            }
+            var vm = new NewCustomerViewModel
+            {
+                Customer = updateCast,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+            return View("New", vm);
+        }
+
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var cust = _context.Customers.Find(id);
+            _context.Customers.Remove(cust);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Customers");
+        }
+
+
+
+
+
         protected override void Dispose(bool disposing)
         {
             _context.Dispose();
         }
+
+
+
     }
 }
--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -13,9 +13,107 @@ namespace MovieCustomerWithAuthMVC_app.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
+        //// GET: Movies
+        //private ApplicationDbContext _context;
+        //// GET: Movie
+        //public MoviesController()
+        //{
+        //    _context = new ApplicationDbContext();
+        //}
+        //public ActionResult Index()
+        //{
+        //    var mov = _context.Movies.Include(c => c.Genre).ToList();
+        //    return View(mov);
+        //}
+        //public ActionResult Details(int id)
+        //{
+        //    var singleMovie = _context.Movies.Include(c => c.Genre).SingleOrDefault(c => c.Id == id);
+        //    if (singleMovie == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(singleMovie);
+        //}
+        //[HttpGet]
+        //public ActionResult New()
+        //{
+        //    var genre = _context.Genres.ToList();
+        //    var viewModel = new NewMovieViewModel
+        //    {
+        //        Genres = genre
+        //    };
+        //    return View(viewModel);
+        //}
+        //[HttpPost]
+        //public ActionResult Create(Movie movie)//Model binding
+        //{
+        //    _context.Movies.Add(movie);
+        //    _context.SaveChanges();
+        //    return RedirectToAction("Index", "Movies");
+        //}
+        //public ActionResult Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    var movieTbl = _context.Movies.Include(c => c.Genre).SingleOrDefault(c => c.Id == id);
+        //    if (movieTbl == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    ViewBag.Genres = new SelectList(_context.Genres, "Id", "GenreName", movieTbl.GenreId);
+        //    return View("New",movieTbl);  //changes here
+        //}
+        /////New Code
+        //[HttpPost]
+        //public ActionResult Save(Movie movie)
+        //{
+        //    if (movie.Id == 0)
+        //        _context.Movies.Add(movie);
+        //    else
+        //    {
+        //        var movieInDb = _context.Movies.Single(c => c.Id == movie.Id);
+        //        movieInDb.MovieName = movie.MovieName;
+        //        movieInDb.Genre = movie.Genre;
+        //    }
+        //    _context.SaveChanges();
+        //    return RedirectToAction("Index", "Movies");
+        //}
+        ////
+        //public ActionResult Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    };
+        //    var movieTbl = _context.Movies.Include(c => c.Genre).SingleOrDefault(c => c.Id == id);
+        //    if (movieTbl == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(movieTbl);
+        //}
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeleteConfirmed(int id)
+        //{
+        //    var movieTbl = _context.Movies.Find(id);
+        //    _context.Movies.Remove(movieTbl);
+        //    _context.SaveChanges();
+        //    return RedirectToAction("Index", "Movies");
+        //}
+        //protected override void Dispose(bool disposing)
+        //{
+        //    _context.Dispose();
+        //}
+
+
+
+
+
         private ApplicationDbContext _context;
-        // GET: Movie
+        // GET: Movies
         public MoviesController()
         {
             _context = new ApplicationDbContext();
@@ -44,52 +142,70 @@ namespace MovieCustomerWithAuthMVC_app.Controllers
             };
             return View(viewModel);
         }
+        //[HttpPost]
+        //public ActionResult Create(Movie movie)//Model binding
+        //{
+        //    _context.Movies.Add(movie);
+        //    _context.SaveChanges();
+        //    return RedirectToAction("Index", "Movies");
+        //}
         [HttpPost]
-        public ActionResult Create(Movie movie)//Model binding
+        public ActionResult Save(Movie movie)
         {
-            _context.Movies.Add(movie);
+            if (movie.Id == 0) _context.Movies.Add(movie);
+            else
+            {
+                var movieInDb = _context.Movies.Single(c => c.Id == movie.Id);
+                movieInDb.MovieName = movie.MovieName;
+                movieInDb.GenreId = movie.GenreId;
+
+
+            }
             _context.SaveChanges();
             return RedirectToAction("Index", "Movies");
         }
-        public ActionResult Edit(int? id)
+        public ActionResult Edit(int id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            var movieTbl = _context.Movies.Include(c => c.Genre).SingleOrDefault(c => c.Id == id);
-            if (movieTbl == null)
+            var updateMov = _context.Movies.SingleOrDefault(c => c.Id == id);
+            if (updateMov == null)
             {
                 return HttpNotFound();
             }
-            ViewBag.Genres = new SelectList(_context.Genres, "Id", "GenreName", movieTbl.GenreId);
-            return View(movieTbl);
+            var vm = new NewMovieViewModel
+            {
+                Movie = updateMov,
+                Genres = _context.Genres.ToList()
+            };
+            return View("New", vm);
         }
         public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            };
-            var movieTbl = _context.Movies.Include(c => c.Genre).SingleOrDefault(c => c.Id == id);
-            if (movieTbl == null)
-            {
-                return HttpNotFound();
             }
-            return View(movieTbl);
-        }
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
             var movieTbl = _context.Movies.Find(id);
             _context.Movies.Remove(movieTbl);
             _context.SaveChanges();
             return RedirectToAction("Index", "Movies");
         }
+
         protected override void Dispose(bool disposing)
         {
             _context.Dispose();
         }
+
+
+
+
+
+
+
+
+
     }
 }
+
+
+
+
